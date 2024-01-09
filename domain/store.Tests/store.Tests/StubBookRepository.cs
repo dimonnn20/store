3c88af78f86f686be676d562aa4ab948b58911a5
dimonnn20 <dimonnn20@gmail.com>
2024-01-09 20:26:00 +0100

    added test without mocko

---
+++ domain/store.Tests/store.Tests/StubBookRepository.cs
@@ -0,0 +1,24 @@
+﻿using Store;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace store.Tests
+{
+    internal class StubBookRepository : IBookRepository
+    {
+        public Book[] ResultOfGetAllByIsbn { get;set; }
+        public Book[] ResultOfGetAllByTitleOrAuthor { get; set; }
+        public Book[] GetAllByIsbn(string isbn)
+        {
+            return ResultOfGetAllByIsbn;
+        }
+
+        public Book[] GetAllByTitleOrAuthor(string titleOrAuthorPart)
+        {
+            return ResultOfGetAllByTitleOrAuthor;
+        }
+    }
+}
